6fe7a2b1639a7a6f886a6a6e7319040792326f8b
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-03-11 18:30:30 -0500

    fix: update automaticCarouselTimer function to improve auto scrolling

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -3,25 +3,25 @@ const images = [
 		id: crypto.randomUUID(),
 		name: 'JavaScript',
 		image:
-			'https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+			'https://images.pexels.com/photos/16592498/pexels-photo-16592498/free-photo-of-internet-conexion-tecnologia-negocio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 	},
 	{
 		id: crypto.randomUUID(),
 		name: 'React',
 		image:
-			'https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+			'https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 	},
 	{
 		id: crypto.randomUUID(),
 		name: 'Desktop',
 		image:
-			'https://images.unsplash.com/photo-1669023414162-8b0573b9c6b2?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+			'https://images.pexels.com/photos/1714202/pexels-photo-1714202.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 	},
 	{
 		id: crypto.randomUUID(),
 		name: 'Python',
 		image:
-			'https://images.unsplash.com/photo-1667372531881-6f975b1c86db?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+			'https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 	},
 ];
 
@@ -33,6 +33,7 @@ const DIRECTION = {
 const TAG_CLASSES = {
 	carousel: 'carousel',
 	container: 'carousel__container',
+	imagesContainer: 'carousel__images-container',
 	images: 'carousel__images',
 	image: 'carousel__image',
 	navegation: 'carousel__navegation',
--- src/carousel/index.js
+++ src/carousel/index.js
@@ -1,117 +1,130 @@
 import { createTag } from '../helper/create-tag';
 import { DIRECTION, TAG_CLASSES, images } from './carousel';
 
+const DOC = window.document;
+const ROOT_STYLES = DOC.documentElement.style;
+const IMAGE_ARRAY_SIZE = images.length;
+
 let CURRENT_IMAGE = 0;
-const IMAGE_ARRAY_SIZE = images.length - 1;
+let active = true;
 
-const imagePreview = ({ imageTag }) => {
-	imageTag.id = images[CURRENT_IMAGE].id;
-	imageTag.title = images[CURRENT_IMAGE].name;
-	imageTag.src = images[CURRENT_IMAGE].image;
-};
+const automaticCarouselTimer = () => {
+	const counter = () => {
+		if (active) {
+			CURRENT_IMAGE++;
 
-const automaticCarouselTimer = ({
-	imageTag,
-	time = 2000,
-	restartCarousel = false,
-}) => {
-	let timerCarousel;
+			if (CURRENT_IMAGE >= IMAGE_ARRAY_SIZE) CURRENT_IMAGE = 0;
+		}
 
-	const startInterval = () => {
-		timerCarousel = setInterval(() => {
-			if (CURRENT_IMAGE < IMAGE_ARRAY_SIZE) {
-				++CURRENT_IMAGE;
-			} else if (CURRENT_IMAGE === IMAGE_ARRAY_SIZE) {
-				CURRENT_IMAGE = 0;
-			}
-
-			imagePreview({ imageTag });
-		}, time);
+		const transform = CURRENT_IMAGE === 0 ? '0%' : `-${CURRENT_IMAGE}00%`;
+		ROOT_STYLES.setProperty('--transform-carousel', transform);
 	};
 
+	const startInterval = () => setInterval(counter, 2000);
+
 	startInterval();
-
-	const restartInterval = () => clearInterval(timerCarousel);
-
-	if (restartCarousel) {
-		restartInterval();
-	}
 };
 
-const handlerImage = ({ direction = 'RIGHT', imageTag }) => {
+const handlerImage = ({ direction = 'RIGHT' }) => {
 	if (direction === DIRECTION.LEFT) {
 		if (CURRENT_IMAGE === 0) {
-			CURRENT_IMAGE = IMAGE_ARRAY_SIZE;
+			CURRENT_IMAGE = IMAGE_ARRAY_SIZE - 1;
 		} else {
 			--CURRENT_IMAGE;
 		}
 	} else if (direction === DIRECTION.RIGHT) {
-		if (CURRENT_IMAGE < IMAGE_ARRAY_SIZE) {
+		if (CURRENT_IMAGE < IMAGE_ARRAY_SIZE - 1) {
 			++CURRENT_IMAGE;
-		} else if (CURRENT_IMAGE === IMAGE_ARRAY_SIZE) {
+		} else if (CURRENT_IMAGE === IMAGE_ARRAY_SIZE - 1) {
 			CURRENT_IMAGE = 0;
 		}
 	}
+	active = false;
 
-	imagePreview({ imageTag });
+	const transform = CURRENT_IMAGE === 0 ? '0%' : `-${CURRENT_IMAGE}00%`;
+	ROOT_STYLES.setProperty('--transform-carousel', transform);
 };
 
-const showCarousel = () => {
-	const btnPrevious = createTag({
-		tag: 'button',
-		id: 'btn-prev',
-		textContent: '<',
-		title: 'Anterior',
-		class: [TAG_CLASSES.btn],
-	});
-	const btnNext = createTag({
-		tag: 'button',
-		textContent: '>',
-		id: 'btn-next',
-		title: 'Siguiente',
-		class: [TAG_CLASSES.btn],
+const btnPrevious = createTag({
+	tag: 'button',
+	id: 'btn-prev',
+	textContent: '<',
+	title: 'Anterior',
+	class: [TAG_CLASSES.btn],
+});
+const btnNext = createTag({
+	tag: 'button',
+	textContent: '>',
+	id: 'btn-next',
+	title: 'Siguiente',
+	class: [TAG_CLASSES.btn],
+});
+const navegation = createTag({
+	tag: 'nav',
+	child: [btnPrevious, btnNext],
+	class: [TAG_CLASSES.navegation],
+});
+
+const fragment = DOC.createDocumentFragment();
+
+ROOT_STYLES.setProperty('--width-image-carousel', `${100 / IMAGE_ARRAY_SIZE}%`);
+
+images.forEach((image) => {
+	const imageTag = createTag({
+		tag: 'img',
+		id: image.id,
+		title: image.name,
+		src: image.image,
+		class: [TAG_CLASSES.image],
 	});
 
-	const imageTag = createTag({ tag: 'img', class: [TAG_CLASSES.image] });
+	fragment.appendChild(imageTag);
+});
 
-	automaticCarouselTimer({ imageTag, time: 3000 });
+const carouselImages = createTag({
+	tag: 'div',
+	child: [fragment],
+	class: [TAG_CLASSES.images],
+});
+
+const imagesContainer = createTag({
+	tag: 'div',
+	child: [carouselImages],
+	class: [TAG_CLASSES.imagesContainer],
+});
+
+const container = createTag({
+	tag: 'div',
+	child: [imagesContainer, navegation],
+	class: [TAG_CLASSES.container],
+});
+
+const carousel = createTag({
+	tag: 'section',
+	child: [container],
+	id: 'carousel',
+	class: [TAG_CLASSES.carousel],
+});
+
+const carouselHandler = () => {
+	ROOT_STYLES.setProperty('--width-carousel', `${IMAGE_ARRAY_SIZE * 100}%`);
+	automaticCarouselTimer();
+
+	imagesContainer.addEventListener('mouseover', () => {
+		if (active) active = false;
+	});
+
+	imagesContainer.addEventListener('mouseout', () => {
+		if (!active) active = true;
+	});
 
 	btnPrevious.addEventListener('click', () =>
-		handlerImage({ direction: DIRECTION.LEFT, imageTag })
+		handlerImage({ direction: DIRECTION.LEFT })
 	);
 
 	btnNext.addEventListener('click', () =>
-		handlerImage({ direction: DIRECTION.RIGHT, imageTag })
+		handlerImage({ direction: DIRECTION.RIGHT })
 	);
-
-	imagePreview({ imageTag });
-
-	const navegation = createTag({
-		tag: 'nav',
-		child: [btnPrevious, btnNext],
-		class: [TAG_CLASSES.navegation],
-	});
-
-	const imagesContainer = createTag({
-		tag: 'div',
-		child: [imageTag],
-		class: [TAG_CLASSES.images],
-	});
-
-	const container = createTag({
-		tag: 'div',
-		child: [imagesContainer, navegation],
-		class: [TAG_CLASSES.container],
-	});
-
-	const carousel = createTag({
-		tag: 'section',
-		child: [container],
-		id: 'carousel',
-		class: [TAG_CLASSES.carousel],
-	});
-
-	return carousel;
 };
 
-export { showCarousel };
+export { carousel, carouselHandler };
